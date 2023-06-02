ebc9e3a0a69acb33253f5df2fa56b7a184fa9a5f
lg <whomsp@gmail.com>
2023-06-02 13:17:07 +0900

    added login feature

--- javascript/momentum_clone/app.js
+++ javascript/momentum_clone/app.js
@@ -1,1 +1,35 @@
-console.log('test');
+const loginForm = document.querySelector('#login-form');
+const loginInput = document.querySelector('#login-form input');
+const greeting = document.querySelector('#greeting');
+
+const HIDDEN_CLASSNAME = 'hidden';
+const USERNAME_KEY = 'newUsername';
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+function onLoginSubmit(e) {
+	e.preventDefault();
+	loginForm.classList.add(HIDDEN_CLASSNAME);
+	const username = loginInput.value;
+	localStorage.setItem(USERNAME_KEY, username);
+	paintGreetings();
+}
+
+function paintGreetings() {
+	const username = localStorage.getItem(USERNAME_KEY);
+	greeting.innerText = `Hello ${username}`;
+	greeting.classList.remove(HIDDEN_CLASSNAME);
+}
+
+if (savedUsername === null) {
+	loginForm.classList.remove(HIDDEN_CLASSNAME);
+	loginForm.addEventListener('submit', onLoginSubmit);
+} else {
+	paintGreetings();
+}
+
+const remove = document.querySelector('.remove');
+remove.addEventListener('click', () => {
+	localStorage.removeItem(USERNAME_KEY);
+	location.reload();
+});
