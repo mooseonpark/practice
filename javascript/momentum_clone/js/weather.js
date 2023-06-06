85e52079a8d366636835984648e5bbcec6629b4f
lg <whomsp@gmail.com>
2023-06-06 16:40:26 +0900

    added weather feature

---
+++ javascript/momentum_clone/js/weather.js
@@ -0,0 +1,27 @@
+const API_KEY = '5e79aa70fc64fd29e78476fe7aa6eb61';
+
+function onGeoOk(position) {
+	const lat = position.coords.latitude;
+	const lon = position.coords.longitude;
+	console.log(lat);
+	console.log(lon);
+	const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+	console.log(url);
+	console.log(url.cod);
+	fetch(url)
+		.then((response) => response.json())
+		.then((data) => {
+			const weather = document.querySelector('#weather span:first-child');
+			const city = document.querySelector('#weather span:last-child');
+			weather.innerText = `${data.weather[0].main} ${Math.floor(
+				data.main.temp
+			)} `;
+			city.innerText = data.name;
+		});
+}
+
+function onGeoError() {
+	alert('cant find where you are, sorry');
+}
+
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
